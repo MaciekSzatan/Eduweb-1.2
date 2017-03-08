a44ac2bf6ddbde5ec04b438c829b20a642d71688
Norbert Mamczura <slage256@gmail.com>
2017-03-08 17:43:31 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+(function($) {
+
+   $(document).ready(function() {
+		$("div.grid.grid-12");
+		
+		$(".nav a[href^='http']");
+		
+		$("input:radio:not(:checked), input:checkbox:not(:checked)");
+		
+		$("div#text p:empty:first");
+		
+		$(".pagination-item:not('span')");
+    });
+
+})(jQuery);
